5046be4cc3c667b7c672c891ea4e551371737cf8
RennAlt <shenprjktplayground@gmail.com>
2025-04-03 18:37:45 +0000

    ysl: touch: Rename to xiaomi_mi8953 and wire up
    thanks @me-cafebabe for refrence
    
    Co-authored-by: me-cafebabe <me.cafebabe@gmail.com>
    Signed-off-by: RenzAlt <129452476+RennAlt@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,9 @@ TARGET_KERNEL_CONFIG := vendor/msm8953-perf_defconfig
 # Display
 TARGET_SCREEN_DENSITY := 280
 
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
 # Kernel
 TARGET_KERNEL_VERSION := 4.19
 
@@ -35,5 +38,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Security Patch Level
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 include vendor/xiaomi/ysl/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -23,6 +23,10 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi_mi8953
+
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
